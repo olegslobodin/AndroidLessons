2b5a9229ea7d34c36084657ff0d576f087ee8c37
Oleg Slobodin <olegslobodin@mail.ru>
2016-01-26 13:40:21 +0300

    fixed

---
+++ p0541_CustomAdapter/src/main/java/ru/olegslobodin/customadapter/MainActivity.java
@@ -0,0 +1,46 @@
+package ru.olegslobodin.customadapter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+    ArrayList<Product> products = new ArrayList<Product>();
+    BoxAdapter boxAdapter;
+
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // создаем адаптер
+        fillData();
+        boxAdapter = new BoxAdapter(this, products);
+
+        // настраиваем список
+        ListView lvMain = (ListView) findViewById(R.id.lvMain);
+        lvMain.setAdapter(boxAdapter);
+    }
+
+    // генерируем данные для адаптера
+    void fillData() {
+        for (int i = 1; i <= 20; i++) {
+            products.add(new Product("Product " + i, i * 1000,
+                    R.mipmap.ic_launcher, false));
+        }
+    }
+
+    // выводим информацию о корзине
+    public void showResult(View v) {
+        String result = "Товары в корзине:";
+        for (Product p : boxAdapter.getBox()) {
+            if (p.box)
+                result += "\n" + p.name;
+        }
+        Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+    }
+}
